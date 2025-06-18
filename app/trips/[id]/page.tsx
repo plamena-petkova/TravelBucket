43edbce44210762c7a50b7ee6fc6b48b35438986
Plamena Petkova <pl.petkova@gmail.com>
2025-06-18 16:26:30 +0300

    Change the way of getting the trip - first find in trips and then api call if neccessary

--- app/trips/[id]/page.tsx
+++ app/trips/[id]/page.tsx
@@ -4,61 +4,60 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import { fetchTrip } from '@/services/tripService';
 import { TripProps } from '@/interfaces/interfaces';
-
+import { useTripsStore } from '@/stores/userStore';
 
 const TripPage = () => {
   const params = useParams();
   const [trip, setTrip] = useState<TripProps | null>(null);
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
+  const trips = useTripsStore((state) => state.trips);
+
   useEffect(() => {
-    const getTrip = async () => {
-      try {
-        const data = await fetchTrip(params.id as string);
-        if (!data) {
-          setError('Trip not found');
-        } else {
-          setTrip(data);
-        }
-      } catch (err) {
-        setError('Failed to fetch trip');
-      } finally {
-        setLoading(false);
-      }
-    };
+    if (!params.id) return;
 
-    if (params?.id) {
-      getTrip();
+    const localTrip = trips?.find((trip) => trip._id === params.id);
+    if (localTrip) {
+      setTrip(localTrip);
+    } else {
+      fetchTrip(params.id as string)
+        .then((data) => {
+          if (!data) {
+            setError('Trip not found');
+          } else {
+            setTrip(data);
+          }
+        })
+        .catch(() => setError('Failed to fetch trip'));
     }
-  }, [params.id]);
+  }, [params.id, trips]);
 
-  if (loading) return <div>Loading trip...</div>;
   if (error) return <div>Error: {error}</div>;
-  if (!trip) return <div>Trip not found.</div>;
+  if (!trip) return <div>Loading trip...</div>;
 
   return (
     <div className="">
       <div className="flex carousel w-full items-center">
-        <div id={trip._id} className="carousel-item w-full h-36">
+        <div id={trip._id} className="carousel-item w-full h-52">
           <img
             src={trip.coverImageUrl}
-            className="w-full object-cover" />
+            className="w-full object-cover"
+            alt={trip.title}
+          />
         </div>
       </div>
       <div className="flex w-full justify-center gap-2 py-2">
         <a href={trip._id} className="btn btn-xs">1</a>
       </div>
       <div className="flex flex-col items-center">
-        <h1>{trip.title}</h1>
+        <h1 className="h1 font-bold">{trip.title}</h1>
         <p>{trip.description}</p>
         <p>From {trip.startDateTrip} to {trip.endDateTrip}</p>
-        <h2>{trip?.destination?.city}</h2>
-        <p>{trip?.destination?.country}</p>
-        <h2>Accomodation</h2>
-        <p>{trip?.accomodation?.type}</p>
+        <h2>{trip.destination?.city}</h2>
+        <p>{trip.destination?.country}</p>
+        <h2>Accommodation</h2>
+        <p>{trip.accomodation?.type}</p>
       </div>
-  
     </div>
   );
 };
